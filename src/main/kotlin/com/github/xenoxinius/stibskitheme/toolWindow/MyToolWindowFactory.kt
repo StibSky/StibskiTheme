86b1855067a41fd0f50b7426c6499bebcc4db3cb
GitHub Action <action@github.com>
2023-04-12 11:02:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/xenoxinius/stibskitheme/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/xenoxinius/stibskitheme/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.xenoxinius.stibskitheme.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xenoxinius.stibskitheme.MyBundle
+import com.github.xenoxinius.stibskitheme.services.MyProjectService
 import javax.swing.JButton
 
 
